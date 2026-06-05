c8eacb58ca326d7e68698f895edb884ebb453712
Zach Lagden <zach@zachlagden.uk>
2026-06-05 11:53:51 +0100

    build: add umami-sdk/vue entry point

--- tsup.config.ts
+++ tsup.config.ts
@@ -18,7 +18,7 @@ async function prependUseClient() {
 
 export default defineConfig([
   {
-    entry: { index: 'src/index.ts', node: 'src/node/index.ts' },
+    entry: { index: 'src/index.ts', node: 'src/node/index.ts', vue: 'src/vue/index.ts' },
     format: ['esm', 'cjs'],
     dts: true,
     clean: true,
